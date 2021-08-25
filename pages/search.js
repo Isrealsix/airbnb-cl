8872c0a9e9cd8dca659cf71c6c7a04447aea6175
Isreal6ix <isrealsix@gmail.com>
2021-08-25 18:41:03 +0700

    added, configured and styled the DateRangePicker component

---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Footer from '../components/Footer';
+
+const Search = () => {
+    return (
+        <div className="">
+            <Header />
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
