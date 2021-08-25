004cb3d6aa466f891d5c9659d9e1958a6d3f1281
Isreal6ix <isrealsix@gmail.com>
2021-08-25 19:30:54 +0700

    used the useRouter to retrieve and display params on the search page

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,24 @@
 import Header from "../components/Header"
 import Footer from '../components/Footer';
+import { useRouter } from 'next/dist/client/router';
+import { format } from "date-fns";
 
 const Search = () => {
+    const router = useRouter();
+    const { location, startDate, endDate, numberOfGuests } = router.query;
+
+    const formattedStartDate = format(new Date(startDate), 'dd MMMM yyyy')
+    const formattedEndDate = format(new Date(endDate), 'dd MMMM yyyy');
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
+    
     return (
         <div className="">
             <Header />
                 <main className="flex">
                     <section className="flex-grow pt-14 px-6">
-                        <p className="text-xs">300+ Stays for 5 number of guests</p>
+                        <p className="text-xs">300+ Stays - {range} -for {numberOfGuests} guests</p>
 
-                        <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in PH</h1>
+                        <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
 
                         <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                             <p className="button">Cancellation Flexibility</p>
