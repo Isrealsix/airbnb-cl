4d21524aa21a9b22f1ac310b41676d7181edeadf
Isreal6ix <isrealsix@gmail.com>
2021-08-25 23:52:18 +0700

    installed geolib, mapped through the results to get all the long and lat, then use the getCenter to grt the center point

--- components/Map.js
+++ components/Map.js
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
-const Map = () => {
+// import { getCenter } from 'geolib/es/getCenter';
+import { getCenter } from 'geolib';
+
+const Map = ({ searchResults }) => {
+	// Transforming the search result
+
+	const coordinates = searchResults?.map(result => ({
+		longitude: result.long,
+		latitude: result.lat,
+	}));
+
+	const center = getCenter(coordinates);
+
 	const [viewport, setViewport] = useState({
 		width: '100%',
 		height: '100%',
-		latitude: 37.7577,
-		longitude: -122.4376,
+		latitude: center.latitude,
+		longitude: center.longitude,
 		zoom: 10,
 	});
+
 	return (
 		<ReactMapGL
 			mapStyle="mapbox://styles/isrealsix/cksrl6bcr28ln17o7kcwtvarc"
 			mapboxApiAccessToken={process.env.mapbox_access_token}
 			{...viewport}
+			onViewportChange={nextViewport => setViewport(nextViewport)}
 		></ReactMapGL>
 	);
 };
--- pages/search.js
+++ pages/search.js
@@ -1,70 +1,86 @@
-import Header from "../components/Header"
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useRouter } from 'next/dist/client/router';
-import { format } from "date-fns";
-import InfoCard from "../components/InfoCard";
+import { format } from 'date-fns';
+import InfoCard from '../components/InfoCard';
 import Map from '../components/Map';
 
-const Search = ({searchResults}) => {
-    const router = useRouter();
-    const { location, startDate, endDate, numberOfGuests } = router.query;
+const Search = ({ searchResults }) => {
+	const router = useRouter();
+	const { location, startDate, endDate, numberOfGuests } = router.query;
 
-    const formattedStartDate = format(new Date(startDate), 'dd MMMM yyyy')
-    const formattedEndDate = format(new Date(endDate), 'dd MMMM yyyy');
-    const range = `${formattedStartDate} - ${formattedEndDate}`;
-    
-    return (
-        <div className="">
-            <Header  placeholder={`${location} | ${range} ${numberOfGuests} guests`} />
+	const formattedStartDate = format(new Date(startDate), 'dd MMMM yyyy');
+	const formattedEndDate = format(new Date(endDate), 'dd MMMM yyyy');
+	const range = `${formattedStartDate} - ${formattedEndDate}`;
 
-                <main className="flex">
-                    <section className="flex-grow pt-14 px-6">
-                        <p className="text-xs">300+ Stays - {range} -for {numberOfGuests} guests</p>
+	return (
+		<div className="">
+			<Header placeholder={`${location} | ${range} ${numberOfGuests} guests`} />
 
-                        <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+			<main className="flex">
+				<section className="flex-grow pt-14 px-6">
+					<p className="text-xs">
+						300+ Stays - {range} -for {numberOfGuests} guests
+					</p>
 
-                        <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-                            <p className="button">Cancellation Flexibility</p>
-                            <p className="button">Type of Place</p>
-                            <p className="button">Price</p>
-                            <p className="button">Rooms and Beds</p>
-                            <p className="button">More filters</p>
-                        </div>
+					<h1 className="text-3xl font-semibold mt-2 mb-6">
+						Stays in {location}
+					</h1>
 
-                        <div className="flex flex-col">
-                        {searchResults.map(({id, img, location, title, description, star, price, total}) => (
-                            <InfoCard 
-                            key={id}
-                            img={img}
-                            location={location}
-                            title={title}
-                            description={description}
-                            star={star}
-                            price={price}
-                            total={total}
-                            />
-                            ))}
-                            </div>
-                    </section>
+					<div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+						<p className="button">Cancellation Flexibility</p>
+						<p className="button">Type of Place</p>
+						<p className="button">Price</p>
+						<p className="button">Rooms and Beds</p>
+						<p className="button">More filters</p>
+					</div>
 
-                    <section className="hidden xl:inline-flex xl:min-w-[600px]">
-                            <Map />
-                    </section>
-                </main>
-            <Footer />
-        </div>
-    )
-}
+					<div className="flex flex-col">
+						{searchResults.map(
+							({
+								id,
+								img,
+								location,
+								title,
+								description,
+								star,
+								price,
+								total,
+							}) => (
+								<InfoCard
+									key={id}
+									img={img}
+									location={location}
+									title={title}
+									description={description}
+									star={star}
+									price={price}
+									total={total}
+								/>
+							)
+						)}
+					</div>
+				</section>
+
+				<section className="hidden xl:inline-flex xl:min-w-[600px]">
+					<Map searchResults={searchResults} />
+				</section>
+			</main>
+			<Footer />
+		</div>
+	);
+};
 
 export default Search;
 
-
 export async function getServerSideProps() {
-    const searchResults = await fetch('https://jsonkeeper.com/b/QX5M').then(res => res.json());
+	const searchResults = await fetch('https://jsonkeeper.com/b/QX5M').then(res =>
+		res.json()
+	);
 
-    return {
-        props: {
-            searchResults
-        }
-    }
+	return {
+		props: {
+			searchResults,
+		},
+	};
 }
